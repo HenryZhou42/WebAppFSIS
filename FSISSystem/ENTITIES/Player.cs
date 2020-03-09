76fc58fc31a173b872d305c71f81a63183aa3e49
HenryZhou42 <59575952+HenryZhou42@users.noreply.github.com>
2020-03-09 16:19:38 -0600

    a

--- FSISSystem/ENTITIES/Player.cs
+++ FSISSystem/ENTITIES/Player.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FSISSystem
+namespace FSISSystem.ENTITIES
 {
-    public class Class1
+    class Player
     {
     }
 }
